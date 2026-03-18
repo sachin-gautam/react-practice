2f93c5af47c78e93a3c7328202c9d9d87bc77dbc
sachin-ukpa <sachin@ukpa.co.uk>
2026-03-18 08:44:06 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am h1"),
+  ),
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from React",
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
